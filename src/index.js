0aaaef20a4e72580d9037beab26facf1d7a072ee
fcork <frankcorkery@gmail.com>
2018-06-25 17:33:48 -0400

    Initial Commit
    - Got index to render app.js instead of error

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 // TODO: Render the `App` component to the DOM
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div></div>;
+  }
+}
 
+ReactDOM.render(<App/>, document.getElementById('app'));
