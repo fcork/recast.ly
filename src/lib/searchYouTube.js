408baf9eba06a24cecc2c258d6c0fbcb1fc5abc5
fcork <frankcorkery@gmail.com>
2018-06-26 12:30:41 -0400

    Begin on searchYouTube
    - TODO: Finish hitting YouTube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      'part': 'snippet',
+      'q': options.query,
+      'maxResults': options.max,
+      'onBehalfOfContentOwner': options.key,
+      'videoEmbeddable': true
+    },
+    success: (data) => {
+      console.log(data);
+      callback(data.items);
+    },
+    error: (data) => {
+      console.log('Error: ', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
